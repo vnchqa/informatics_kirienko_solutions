eb2e06e03a28355d3f350b3178fd65f4ca9d4ed2
vnchqa <154042517+vnchqa@users.noreply.github.com>
2025-02-01 09:09:09 +0300

    Create Задача C.py Reformat code

---
+++ 2 раздел/Задача C.py
@@ -0,0 +1,6 @@
+number = int(input())
+previous_num = number - 1
+next_num = number + 1
+
+print(f'The next number for the number {number} is {next_num}.')
+print(f'The previous number for the number {number} is {previous_num}.')
